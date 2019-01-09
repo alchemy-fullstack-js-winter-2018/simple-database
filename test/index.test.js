601997346e9dec2dced6c54e63f51f0f34867827
Kate Dameron <katerj33@gmail.com>
2019-01-09 15:06:19 -0800

    create store passing test

--- test/index.test.js
+++ test/index.test.js
@@ -5,31 +5,32 @@ const Store = require('../lib/index');
 
 
 describe('Store', () => {
-
+    let store = null;
     beforeEach(done => {
-        //clear out all files before you start testing using rimraf node module
+        //clear out all files before you start testing using rimraf node module then call done
         rimraf('./testData/store', err => {
             done(err);
         })
     });
 
-    beforeEach(() => {
-        //make new directories using mkdirp node module
+    beforeEach(done => {
+        //make new directories using mkdirp node module then call done
         mkdirp('./testData/store', err => {
             done(err);
         });
     });
 
     beforeEach(() => {
-        //tell it where to save stuff
+        //tell it where to save stuff 
         store = new Store('./testData/store');
-    })
+    });
 
     it('creates an object in my store', done => {
         //use the create function to make a new object in the store - return a falsy error and return the new object
-        store.create({ }, (err, createdObj) => {
+        store.create({ snack: 'plaintain chips' }, (err, createdSnack) => {
             expect(err).toBeFalsy();
-            expect(createObj).toEqual({ _id: expect.any(String) });
+            expect(createdSnack).toEqual({ snack: 'plaintain chips', _id: expect.any(String) });
+            done(err);
         });
     });
 });
