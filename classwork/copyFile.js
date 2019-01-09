f274ffcb28794b4aea898d1edcf203194e369c19
Sophie Graham <sophiergraham1@gmail.com>
2019-01-09 15:04:40 -0800

    deleted classwork file, added to index js, test failing

--- classwork/copyFile.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('./LAB.md', {encoding: 'utf8' }, (err, data) => {
-  if (err) throw `Error reading file: ${err}`;
-  fs.writeFile('./LAB-copy.md', data, err => {
-    if (err) throw err;
-  });
-});
