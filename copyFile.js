49d441fde5c12d1592f1039b84265554e6119b9d
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-09 10:12:39 -0800

    shorten return statement

--- copyFile.js
+++ copyFile.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // creates new lab copy and writes contents of lab.md to it
 fs.readFile('./LAB.md', { encoding: 'utf8' }, (err, data) => {
   if(err) throw err;
-  return data = fs.writeFile('LAB-copy.md', data, err => {
+  return fs.writeFile('LAB-copy.md', data, err => {
     if(err) throw err;
   });
 });
