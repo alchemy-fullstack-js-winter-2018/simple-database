d7bb4ff0515953c9afcc125fce3cc70d8cf7840e
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-09 17:56:04 -0800

    more travis things

--- morningStuff/copyfile.js
+++
@@ -1,5 +0,0 @@
-const fsPromises = require('fs').promises;
-
-fsPromises.copyFile('./LAB.md', 'CopyLab.txt')
-    .then(() => console.log('file has been copied'))
-    .catch(() => console.log('there was an error'));
--- morningStuff/writefile.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const stuff = new Uint8Array(Buffer.from('Hello Node World .js'));
-fs.writeFile('message.txt', stuff, (err) => {
-    if(err) throw err;
-    console.log('file has be written');
-});
