130e32a33017f36772e0f5b92250fce3e18c1849
Ivan <i.poblete915@gmail.com>
2019-01-09 22:08:53 -0800

    find all objects tracked by store passed test

--- test/index.test.js
+++ test/index.test.js
@@ -39,19 +39,19 @@ describe('Store', () => {
   });
 
   it('find all objects tracked by the store', done => {
-    store.create({ dog: 1 }, (err, item1) => {
-      store.create({ dog: 2 }, (err, item2) => {
-        store.create({ dog: 3 }, (err, item3) => {
-          store.create({ dog: 4 }, (err, item4) => {
-            store.create({ dog: 5 }, (err, item5) => {
+    store.create({ dog: 1 }, (err, dog1) => {
+      store.create({ dog: 2 }, (err, dog2) => {
+        store.create({ dog: 3 }, (err, dog3) => {
+          store.create({ dog: 4 }, (err, dog4) => {
+            store.create({ dog: 5 }, (err, dog5) => {
               store.find((err, listOfDogs) => {
                 expect(err).toBeFalsy();
                 expect(listOfDogs).toHaveLength(5);
-                expect(listOfDogs).toContainEqual(item1);
-                expect(listOfDogs).toContainEqual(item2);
-                expect(listOfDogs).toContainEqual(item3);
-                expect(listOfDogs).toContainEqual(item4);
-                expect(listOfDogs).toContainEqual(item5);
+                expect(listOfDogs).toContainEqual(dog1);
+                expect(listOfDogs).toContainEqual(dog2);
+                expect(listOfDogs).toContainEqual(dog3);
+                expect(listOfDogs).toContainEqual(dog4);
+                expect(listOfDogs).toContainEqual(dog5);
                 done();
               });
             });
