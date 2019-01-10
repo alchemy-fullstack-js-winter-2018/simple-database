250b9f41a4c82bc963d8640162e57c7b705cb914
Sophie Graham <sophiergraham1@gmail.com>
2019-01-09 18:35:07 -0800

    fixed linter errors and passing 4 tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
-const mkdirp = require ('mkdir');
+const mkdirp = require ('mkdirp');
 const rimraf = require('rimraf');
+const Store = require('../lib/index');
 
 describe('Store', () => {
   let store = null;
@@ -16,14 +17,61 @@ describe('Store', () => {
   });
   
   beforeEach(() => {
-    store = new store('./testData/store');
+    store = new Store('./testData/store');
   });
 
   it('creates an object in my store', done => {
-    store.create({ name:'ryan' }, (err, createdPerson) => {
+    store.create({ name: 'ryan' }, (err, createdPerson) => {
       expect(err).toBeFalsy();
       expect(createdPerson).toEqual({ name: 'ryan', _id: expect.any(String) });
       done();
+    });
+  });
+
+  it('finds an object by id', done => {
+    store.create({ name: 'sophie' }, (err, createdPerson) => {
+      store.findById(createdPerson._id, (err, foundPerson) => {
+        expect(err).toBeFalsy();
+        expect(foundPerson).toEqual({ name: 'sophie', _id: createdPerson._id });
+        done();
+      });
+    });
+  });
+
+  it('find all objects tracked by the store', done => {
+    store.create({ name: 'ryan' }, (err, person1) => {
+      store.create({ name: 'mariah' }, (err, person2) => {
+        store.create({ name: 'kevin' }, (err, person3) => {
+          store.create({ name: 'marty' }, (err, person4) => {
+            store.create({ name: 'shannon' }, (err, person5) => {
+              store.find((err, listOfPeople) => {
+                expect(err).toBeFalsy();
+                expect(listOfPeople).toHaveLength(5);
+                expect(listOfPeople).toContainEqual(person1);
+                expect(listOfPeople).toContainEqual(person2);
+                expect(listOfPeople).toContainEqual(person3);
+                expect(listOfPeople).toContainEqual(person4);
+                expect(listOfPeople).toContainEqual(person5);
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+  
+  it('deletes an object with an id', done => {
+    store.create({ person: 'I am going to delete' }, (err, createdPerson) => {
+      store.findByIdAndDelete(createdPerson._id, (err, result) => {
+        expect(err).toBeFalsy();
+        expect(result).toEqual({ deleted:1 });
+        store.findById(createdPerson._id, (err, foundPerson) => {
+          expect(err).toBeTruthy();
+          expect(foundPerson).toBeFalsy();
+          done();
+        });
+      });
     });
   });
 });
