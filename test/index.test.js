ff865647588c38485aa96c1ebe1a058997ed7c90
Kristin Hortsch <kristinhortsch@gmail.com>
2019-01-09 16:51:19 -0800

    delete is working

--- test/index.test.js
+++ test/index.test.js
@@ -39,4 +39,32 @@ describe('Store', () => {
 
     });
   });
+
+  it('finds an object in the store by id and deletes it', done => {
+    store.create({ movie: 'Home Alone' }, (err, createdMovie) => {
+      store.findByIdAndDelete(createdMovie._id, (err, deletedMovie) => {
+        expect(err).toBeFalsy();
+        expect(deletedMovie).toEqual({ deleted: 1 });
+        done();
+      });
+    });
+  });
+
+  // it('can return an array of all the objects in the directory', () => {
+  //   store.create({ movie: 'Air Bud' }, (err, createdMovie) => {
+  //     store.create({ movie: 'Home Alone' }, (err, createdMovie) => {
+  //       store.create({ movie: 'Lion King' }, (err, createdMovie) => {
+  //         store.create({ movie: 'Sword In The Stone'}, (err, createdMovie) => {
+  //           store.create({ movie: 'Little Mermaid'}, (err, createdMovie) => {
+  //             store.find((err, allItems) => {
+  //               expect(err).toBeFalsy();
+  //               expect(allItems).toEqual([{ movie: 'Home Alone' }, { movie: 'Pirates' }, { movie: 'Serendipity' }]);
+
+  //             });
+  //           });
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
 });
