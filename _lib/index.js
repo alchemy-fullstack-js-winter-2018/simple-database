b1e9613b44baf122275eb216b4096a65e66b1907
cruzayye <johnnyc010392@gmail.com>
2019-01-10 17:57:59 -0800

    adjusted spaces

--- _lib/index.js
+++ _lib/index.js
@@ -1,39 +1,33 @@
 const fs = require('fs');
 const shortid = require('shortid');
 
-
-
-
-
 class Store {
     constructor(rootDir) {
         this.rootDir = rootDir;
     }
-    //callback is for once your done with creating file 
+    //NOTETOSELF:callback is for once your done with creating file 
     create(obj, callback) {
         const _id = shortid.generate();
         const copiedObj = { ...obj, _id };
         const objWithStrID = JSON.stringify(copiedObj);
         fs.writeFile(`${this.rootDir}/${_id}`, objWithStrID, err => {
             if(err) return callback(err);
-            //to user we want to return the copied obj and not the stringified version.
+            //NOTETOSELF: to user we want to return the copied obj and not the stringified version.
             callback(null, copiedObj);
 
         })
 
     }
     findById(_id, callback){
-        //readJson
         fs.readFile(this.storedFilePath(_id), { encoding: 'utf8' }, (err, data) =>{
-            //DONT forget to throw something in case file cant be found!!
+            //NOTETOSELF: DONT forget to throw something in case file cant be found!!
             if(err) return callback(err);
             const parsedJson = JSON.parse(data);
             callback(err, parsedJson);
-            //in case the JSON doesnt read  enclose the above in a try catch block;
+            //NOTETOSELF: in case the JSON doesnt read  enclose the above in a try catch block;
         })  
     }
     
-
     findAndDelete(_id, callback) {
         fs.unlink(this.storedFilePath(_id), err => {
             return callback(err, { deleted: 1 });
@@ -42,11 +36,7 @@ class Store {
     storedFilePath(_id) {
         return `${this.rootDir}/${_id}`
       }
-
 }
-
-
-
 module.exports = { 
     Store
 }
