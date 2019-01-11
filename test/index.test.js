10286d56ec8fbdaf997e3cbe859d353d11e83342
JeiWolfei <jvampy22@gmail.com>
2019-01-10 20:56:25 -0800

    fixed bugs and passing tests

--- test/index.test.js
+++ test/index.test.js
@@ -19,78 +19,74 @@ describe('Store', () => {
   beforeEach(() => {
     store = new Store('./testData/store');
   });
-  
+
   it('creates an object in my store', done => {
-    store.create({ name: 'brutus' }, (err, createdDog) => {
+    store.create({ name: 'ryan' }, (err, createdPerson) => {
       expect(err).toBeFalsy();
-      expect(createdDog).toEqual({ name: 'brutus', _id: expect.any(String) });
+      expect(createdPerson).toEqual({ name: 'ryan', _id: expect.any(String) });
       done();
-    });  
+    });
   });
 
-  it('can find an object by id', (done)=> {
-    store.create({ name: 'Lilo' }, (err, createdDog) => {
-      store.findById(createdDog._id, (err, foundDog) => {
+  it('finds an object by id', done => {
+    store.create({ name: 'uncle bob' }, (err, createdUncle) => {
+      store.findById(createdUncle._id, (err, foundUncle) => {
         expect(err).toBeFalsy();
-        expect(foundDog).toEqual({ name: 'Lilo', _id: createdDog._id });
+        expect(foundUncle).toEqual({ name: 'uncle bob', _id: createdUncle._id });
         done();
       });
     });
   });
 
-  it('throws an error when there is no obj at that id', () => {
-    store.findById(1234, (err) => {
-      expect(err).toBeTruthy();
-    });
-  });
-
-  it('updates an existing object', (done) => {
-    store.create({ name: 'Flido' }, (err, createdDog) => {
-      store.findByIdAndUpdate(createdDog._id, { name: 'Fido' }, (err, updatedWithoutTypo) => {
-        expect(err).toBeFalsy();
-        expect(updatedWithoutTypo).toEqual({ name: 'Fido', _id: createdDog._id });
-        store.findById(createdDog._id, (err, foundDog) => {
-          expect(foundDog).toEqual(updatedWithoutTypo);
-          done();
-        });
-
-      });
-
-    });
-  });
-
-  it('can find an object by id and delete it', done => {
-    store.create({ name: 'Thor' }, (err, createdDog) => {
-      store.findById(createdDog._id, (err, foundDog) => {
-        store.findByIdAndDelete(foundDog._id, (err, removedDog) => {
-          expect(err).toBeFalsy();
-          expect(removedDog).toEqual({ deleted: 1 });
-          done();
-        });
-      });
-    });
-  });
-
-  it('can find all objects tracked by the store', done => {
-    store.create({ name: 'Snooky' }, (err, dog1) => {
-      store.create({ name: 'Fido' }, (err, dog2) => {
-        store.create({ name: 'Spot' }, (err, dog3) => {
-          store.create({ name: 'Rufus' }, (err, dog4) => {
-            store.create({ name: 'Marley' }, (err, dog5) => {
+  it('find all objects tracked by the store', done => {
+    store.create({ item: 1 }, (err, item1) => {
+      store.create({ item: 2 }, (err, item2) => {
+        store.create({ item: 3 }, (err, item3) => {
+          store.create({ item: 4 }, (err, item4) => {
+            store.create({ item: 5 }, (err, item5) => {
               store.find((err, listOfItems) => {
                 expect(err).toBeFalsy();
                 expect(listOfItems).toHaveLength(5);
-                expect(listOfItems).toContainEqual(dog1);
-                expect(listOfItems).toContainEqual(dog2);
-                expect(listOfItems).toContainEqual(dog3);
-                expect(listOfItems).toContainEqual(dog4);
-                expect(listOfItems).toContainEqual(dog5);
+                expect(listOfItems).toContainEqual(item1);
+                expect(listOfItems).toContainEqual(item2);
+                expect(listOfItems).toContainEqual(item3);
+                expect(listOfItems).toContainEqual(item4);
+                expect(listOfItems).toContainEqual(item5);
                 done();
               });
             });
           });
         });
       });
-    });       
+    });
+   
+  });
+
+  it('deletes an object with an id', done => {
+    store.create({ item: 'I am going to delete' }, (err, createdItem) => {
+      store.findByIdAndDelete(createdItem._id, (err, result) => {
+        expect(err).toBeFalsy();
+        expect(result).toEqual({ deleted: 1 });
+        store.findById(createdItem._id, (err, foundItem) => {
+          expect(err).toBeTruthy();
+          expect(foundItem).toBeFalsy();
+          done();
+        });
+      });
+    });
+  });
+
+  it('updates an existing object', done => {
+    store.create({ name: 'rayn' }, (err, typoCreated) => {
+      store.findByIdAndUpdate(typoCreated._id, { name: 'ryan' }, (err, updatedWithoutTypo) => {
+        expect(err).toBeFalsy();
+        expect(updatedWithoutTypo).toEqual({ name: 'ryan', _id: typoCreated._id });
+        store.findById(typoCreated._id, (err, foundObj) => {
+          expect(foundObj).toEqual(updatedWithoutTypo);
+          done();
+        });
+
+      });
+    });
   });
 });
