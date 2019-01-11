9e10e9a88cf152ff27a11a52bc5c75f829ace904
JeiWolfei <jvampy22@gmail.com>
2019-01-10 20:06:03 -0800

    deleted classwork

--- readFile.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('./LAB.md', { encoding: 'utf8' }, (err, data) => {
-  console.log(data);
-});
--- writeFile.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-fs.writeFile('./writing.txt', 'I am writing', err => {
-  if (err) {
-    throw err;
-  }
-});
