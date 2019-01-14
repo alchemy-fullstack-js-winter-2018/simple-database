d42116b743b7daa20fcc7fe22de8a87826fed28d
pickuppatterns <pickup.patterns@gmail.com>
2019-01-13 18:10:13 -0800

    updates failing tests

--- test/index.test.js
+++ test/index.test.js
@@ -27,20 +27,21 @@ describe('Store', () => {
 
     });
   });
-
+//Find Object by ID
   it('finds an object by id', done => {
     // create an object
-    store.create({ name: 'scratchy' }, (err, createdScratchy) => {
+    store.create({ name: 'scratchy' }, (err, createdPerson) => {
       // after done creating -> findById
-      store.findById(createdScratchy._id, (err, foundScratchy) => {
+      store.findById(createdPerson._id, (err, foundPerson) => {
         // after found check that it is the same one that we created
         expect(err).toBeFalsy();
-        expect(foundScratchy).toEqual({ name: 'scratchy', _id: createdScratchy._id });
+        expect(foundPerson).toEqual({ name: 'scratchy', _id: createdPerson._id });
         // then call done
         done();
       });
     });
   });
+  
   it('find all objects tracked by the store', done => {
     store.create({ item: 1 }, (err, item_01) => {
       store.create({ item: 1 }, (err, item_02) => {
@@ -63,29 +64,41 @@ describe('Store', () => {
       });
     });
   });
-// it('deletes an object with an id', done => {
-//   //create an item in
-//   delete that item 
-//   findbyid(itfromcreated item)
-//   expect(foundItem).toBeFalsy()
-// })
+
+  //Find By ID and Delete:
+it('deletes an object with an id', done => {
+  // create an object
+  store.create({ name: 'scratchy' }, (err, createdScratchy) => {
+    // after done creating -> findById
+    store.findById(createdScratchy._id, (err, foundScratchy) => {
+      // after found check that it is the same one that we created & delete
+      store.findByIdAndDelete(foundScratchy._id, (err, removedScratchy) => {
+      expect(err).toBeFalsy();
+      expect(removedScratchy).toEqual({ deleted: 1 });
+      // then call done
+      done();
+      });
+    });
+  });  
+})
 
 //Find By ID and Update:
 it('updates an existing object', () => {
   //call store.create
   store.create( { name: 'itchy' }, (err, updateItchy) => {
     // -> store.findByIdAndUpdate(_id, objectToUpdate, callback(error, updatedObject))
-    store.findByIdAndUpdate(updateItchy._id,{ name: 'Scratchy' }, (error, updatedToScratchy) => {
+    store.findByIdAndUpdate(updateItchy._id,{ name: 'Scratchy' }, (error, updatedScratchy) => {
       //-> -> expect updatedObject returned in callback
       expect(err).toBeFalsy();
-      expect(updatedToScratchy).toEqual({ name: 'scratchy', _id: updateItchy_.id });
+      expect(updatedScratchy).toEqual({ name: 'Scratchy', _id: updateItchy._id });
     //-> -> store.findById(createObject._id)
-    store.findById(updatedToScratchy._id, (err, foundScratchy))
+    store.findById(updatedScratchy._id, (err, foundScratchy) => {
     //-> -> -> expect updated object
-    expect(foundScratchy).toEqual(updatedToScratchy);
+    expect(foundScratchy).toEqual(updatedScratchy);
     done();
+    });
     })
-});
+  });
 });
 
 }) 
